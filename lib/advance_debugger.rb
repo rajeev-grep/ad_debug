61f401d6d2bf0def838d3091b11d9fc377d66e59
rajeev <rajeev@grepruby.com>
2013-02-02 16:35:30 +0530

    add while llop

--- lib/advance_debugger.rb
+++ lib/advance_debugger.rb
@@ -4,6 +4,12 @@ module AdvanceDebugger
 
   def self.hello
     puts "Hello Grep Ruby!"
+    puts "Press c for continue"
+begin 
+  val = gets.chomp
+end while val != 'c'
+puts "hello"
+
   end
   
 end
